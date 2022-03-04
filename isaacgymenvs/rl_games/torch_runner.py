eddf73410fd9f258a9310643a27185a4217d86f1
zainzh <395282340@qq.com>
2022-03-04 07:12:43 +0000

    first commit

---
+++ isaacgymenvs/rl_games/torch_runner.py
@@ -0,0 +1,104 @@
+import numpy as np
+import copy
+import torch
+import yaml
+
+from rl_games import envs
+from rl_games.common import object_factory
+from rl_games.common import env_configurations
+from rl_games.common import experiment
+from rl_games.common import tr_helpers
+
+from rl_games.algos_torch import model_builder
+from rl_games.algos_torch import a2c_continuous
+from rl_games.algos_torch import a2c_discrete
+from rl_games.algos_torch import players
+from rl_games.common.algo_observer import DefaultAlgoObserver
+from rl_games.algos_torch import sac_agent
+from rl_games.algos_torch import cql_agent
+
+def _restore(agent, args):
+    if args['checkpoint'] is not None:
+        agent.restore(args['checkpoint'])
+
+def _override_sigma(agent, args):
+    if args['sigma'] is not None:
+        net = agent.model.a2c_network
+        if hasattr(net, 'sigma') and hasattr(net, 'fixed_sigma'):
+            if net.fixed_sigma:
+                with torch.no_grad():
+                    net.sigma.fill_(float(args['sigma']))
+            else:
+                print('Print cannot set new sigma because fixed_sigma is False')
+class Runner:
+    def __init__(self, algo_observer=None):
+        self.algo_factory = object_factory.ObjectFactory()
+        self.algo_factory.register_builder('a2c_continuous', lambda **kwargs : a2c_continuous.A2CAgent(**kwargs))
+        self.algo_factory.register_builder('a2c_discrete', lambda **kwargs : a2c_discrete.DiscreteA2CAgent(**kwargs)) 
+        self.algo_factory.register_builder('sac', lambda **kwargs: sac_agent.SACAgent(**kwargs))
+        #self.algo_factory.register_builder('dqn', lambda **kwargs : dqnagent.DQNAgent(**kwargs))
+        self.algo_factory.register_builder('cql', lambda **kwargs : cql_agent.CQLAgent(**kwargs))   #add cql builder
+
+        self.player_factory = object_factory.ObjectFactory()
+        self.player_factory.register_builder('a2c_continuous', lambda **kwargs : players.PpoPlayerContinuous(**kwargs))
+        self.player_factory.register_builder('a2c_discrete', lambda **kwargs : players.PpoPlayerDiscrete(**kwargs))
+        self.player_factory.register_builder('sac', lambda **kwargs : players.SACPlayer(**kwargs))
+        #self.player_factory.register_builder('dqn', lambda **kwargs : players.DQNPlayer(**kwargs))
+        self.player_factory.register_builder('cql', lambda **kwargs : players.CQLPlayer(**kwargs)) #add cql builder
+
+        self.algo_observer = algo_observer if algo_observer else DefaultAlgoObserver()
+        torch.backends.cudnn.benchmark = True
+
+    def load_config(self, params):
+        self.seed = params.get('seed', None)
+
+        self.algo_params = params['algo']
+        self.algo_name = self.algo_params['name']
+        self.exp_config = None
+
+        if self.seed:
+            torch.manual_seed(self.seed)
+            torch.cuda.manual_seed_all(self.seed)
+            np.random.seed(self.seed)
+
+        config = params['config']
+        config['reward_shaper'] = tr_helpers.DefaultRewardsShaper(**config['reward_shaper'])
+        if 'features' not in config:
+            config['features'] = {}
+        config['features']['observer'] = self.algo_observer     # here algo_observer
+        self.params = params
+
+    def load(self, yaml_conf):
+        self.default_config = yaml_conf['params']
+        self.load_config(params=copy.deepcopy(self.default_config))
+
+    def run_train(self, args):
+        print('Started to train')
+        agent = self.algo_factory.create(self.algo_name, base_name='run', params=self.params)
+        _restore(agent, args)
+        _override_sigma(agent, args)
+        agent.train()
+
+    def run_play(self, args):
+        print('Started to play')
+        player = self.create_player()
+        _restore(player, args)
+        _override_sigma(player, args)
+        player.run()
+
+    def create_player(self):
+        return self.player_factory.create(self.algo_name, params=self.params)
+
+    def reset(self):
+        pass
+
+    def run(self, args):
+        load_path = None
+
+        if args['train']:
+            self.run_train(args)
+
+        elif args['play']:
+            self.run_play(args)
+        else:
+            self.run_train(args)
