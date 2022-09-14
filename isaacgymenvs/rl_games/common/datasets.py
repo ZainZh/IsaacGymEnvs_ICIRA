c6526cba74343b8922461548b7afa54962ba89ff
ZZain <395282340@qq.com>
2022-09-14 23:21:28 +0800

    Dataset

--- isaacgymenvs/rl_games/common/datasets.py
+++ isaacgymenvs/rl_games/common/datasets.py
@@ -3,6 +3,7 @@ import copy
 from torch.utils.data import Dataset
 import configparser
 
+
 class PPODataset(Dataset):
     def __init__(self, batch_size, minibatch_size, is_discrete, is_rnn, device, seq_len):
         self.is_rnn = is_rnn
@@ -28,6 +29,54 @@ class PPODataset(Dataset):
         end = self.last_range[1]
         self.values_dict['mu'][start:end] = mu
         self.values_dict['sigma'][start:end] = sigma
+
+    def __len__(self):
+        return self.length
+
+    def _get_item_rnn(self, idx):
+        gstart = idx * self.num_games_batch
+        gend = (idx + 1) * self.num_games_batch
+        start = gstart * self.seq_len
+        end = gend * self.seq_len
+        self.last_range = (start, end)
+        input_dict = {}
+        for k, v in self.values_dict.items():
+            if k not in self.special_names:
+                if v is dict:
+                    v_dict = {kd: vd[start:end] for kd, vd in v.items()}
+                    input_dict[k] = v_dict
+                else:
+                    if v is not None:
+                        input_dict[k] = v[start:end]
+                    else:
+                        input_dict[k] = None
+
+        rnn_states = self.values_dict['rnn_states']
+        input_dict['rnn_states'] = [s[:, gstart:gend, :].contiguous() for s in rnn_states]
+
+        return input_dict
+
+    def _get_item(self, idx):
+        start = idx * self.minibatch_size
+        end = (idx + 1) * self.minibatch_size
+        self.last_range = (start, end)
+        input_dict = {}
+        for k, v in self.values_dict.items():
+            if k not in self.special_names and v is not None:
+                if type(v) is dict:
+                    v_dict = {kd: vd[start:end] for kd, vd in v.items()}
+                    input_dict[k] = v_dict
+                else:
+                    input_dict[k] = v[start:end]
+
+        return input_dict
+
+    def __getitem__(self, idx):
+        if self.is_rnn:
+            sample = self._get_item_rnn(idx)
+        else:
+            sample = self._get_item(idx)
+        return sample
 
 
 class DatasetList(Dataset):
